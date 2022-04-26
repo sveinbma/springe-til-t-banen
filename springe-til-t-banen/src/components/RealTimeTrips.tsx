c63edb2d36c36da7c9c298c080adef29f268673b
Svein Manum <svein.manum@gmail.com>
2022-04-26 15:57:02 +0200

    Legger til bytt

--- springe-til-t-banen/src/components/RealTimeTrips.tsx
+++ springe-til-t-banen/src/components/RealTimeTrips.tsx
@@ -4,8 +4,13 @@ import { getDepartures } from '../clients/enturClient';
 import { getTimePart } from '../utils/dateUtils';
 import { LiveClock } from './LiveClock';
 
+const BolerStopId = 'NSR:StopPlace:5862';
+const GronlandStopId = 'NSR:StopPlace:6488';
+
 export const RealTimeTrips = () => {
-  const { data: departures } = useSWR('trips', () => getDepartures('NSR:StopPlace:5862', 'NSR:StopPlace:6488'));
+  const [departureStop, setDepartureStop] = React.useState(BolerStopId);
+  const [arrivalStop, setArrivalStop] = React.useState(GronlandStopId);
+  const { data: departures } = useSWR('trips', () => getDepartures(departureStop, arrivalStop));
   if (!departures) {
     return <p>Laster data...</p>;
   }
@@ -14,6 +19,11 @@ export const RealTimeTrips = () => {
   }
   return <div>
     <h2>Avganger fra {departures[0].quay?.name}</h2>
+    <button onClick={() => {
+      const oldDeparture = departureStop;
+      setDepartureStop(arrivalStop);
+      setArrivalStop(oldDeparture);
+    }}>Bytt</button>
     <LiveClock />
     {departures.map((departure, i) => {
       const plannedDepartureTime = departure.aimedDepartureTime;
